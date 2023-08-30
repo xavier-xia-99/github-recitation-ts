74bb209679ae7030ff95daf40cca6774f1c4ed92
Xavier <boxia@andrew.cmu.edu>
2023-08-30 13:30:30 -0400

    added number type

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -5,8 +5,8 @@ import fibonacci from "./fib";
 export default (req: Request, res: Response) => {
   const { num  } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const fibN : number = fibonacci(parseInt(num));
+  let result : string = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
